972ecd99bcccb2063430370b97612d56ee347d60
Anand <sreenidhi707@gmail.com>
2016-11-22 22:22:37 -0800

    Minor Changes

--- ROI_Extracter/ROI_Extracter/dataset.h
+++ ROI_Extracter/ROI_Extracter/dataset.h
@@ -4,10 +4,12 @@
 
 using namespace std;
 
-const string dataset_folder = "C:/Users/sanand2/Dropbox/MyProjects/Computer_Vision/ROI_Extracter/ROI_Extracter/dataset/";
+//const string dataset_folder = "C:/Users/sanand2/Dropbox/MyProjects/Computer_Vision/ROI_Extracter/ROI_Extracter/dataset/";
 const string output_folder = "C:/Users/sanand2/Dropbox/MyProjects/Computer_Vision/ROI_Extracter/ROI_Extracter/";
 
+const string dataset_folder = "C:/Users/sanand2/Google Drive/Projects/Faces/my_faces/";
+
 const uint32_t NUMBER_OF_FILES = 2;
-const string file_names[NUMBER_OF_FILES] = {	"img_00003.jpg",
-												"img_00010.jpg" };
+const string file_names[NUMBER_OF_FILES] = {	"3.jpg",
+												"217.jpg" };
 
