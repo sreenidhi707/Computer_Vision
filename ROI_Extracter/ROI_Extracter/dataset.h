ab72661634cd19563fc4f6f10b864a6006a822e9
Anand <sreenidhi707@gmail.com>
2016-11-23 23:08:12 -0800

    1. Working face detector 2. Working cascade trainer

--- ROI_Extracter/ROI_Extracter/dataset.h
+++ ROI_Extracter/ROI_Extracter/dataset.h
@@ -11,7 +11,4 @@ const string annotations_file_path = "C:/Users/sanand2/Dropbox/MyProjects/Comput
 const string info_file_path = "C:/Users/sanand2/Dropbox/MyProjects/Computer_Vision/ROI_Extracter/ROI_Extracter/dataset/images.txt";
 //const string dataset_folder = "C:/Users/sanand2/Google Drive/Projects/Faces/my_faces/";
 
-const uint32_t NUMBER_OF_FILES = 2;
-const string file_names[NUMBER_OF_FILES] = {	"3.jpg",
-												"217.jpg" };
 
--- face_detection/face_detection.cpp
+++ face_detection/face_detection.cpp
@@ -3,105 +3,95 @@
 
 #include "stdafx.h"
 
-#include <opencv2/imgproc.hpp>  // Gaussian Blur
-#include <opencv2/core.hpp>        // Basic OpenCV structures (cv::Mat, Scalar)
-#include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>  // OpenCV window I/O
-#include <opencv2/features2d.hpp>
-#include <opencv2/objdetect.hpp>
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
+#include <iostream>
 #include <stdio.h>
-#include <string>
-#include <vector>
 
 using namespace std;
 using namespace cv;
 
-const string WindowName = "Face Detection example";
+/** Function Headers */
+void detectAndDisplay(Mat frame);
 
-class CascadeDetectorAdapter : public DetectionBasedTracker::IDetector
+/** Global variables */
+String face_cascade_name = "C://opencv//build//etc//haarcascades//haarcascade_frontalface_alt.xml";
+String eyes_cascade_name = "C://opencv//build//etc//haarcascades//haarcascade_eye_tree_eyeglasses.xml";
+CascadeClassifier face_cascade;
+CascadeClassifier eyes_cascade;
+string window_name = "Capture - Face detection";
+RNG rng(12345);
+
+/** @function main */
+int main(int argc, const char** argv)
 {
-public:
-	CascadeDetectorAdapter(cv::Ptr<cv::CascadeClassifier> detector) :
-		IDetector(),
-		Detector(detector)
+	VideoCapture cap(0);
+	Mat frame;
+
+	//-- 1. Load the cascades
+	if (!face_cascade.load(face_cascade_name)) { printf("--(!)Error loading\n"); return -1; };
+	if (!eyes_cascade.load(eyes_cascade_name)) { printf("--(!)Error loading\n"); return -1; };
+
+	//-- 2. Read the video stream
+	
+	if (1)
 	{
-		CV_Assert(detector);
-	}
-
-	void detect(const cv::Mat &Image, std::vector<cv::Rect> &objects)
-	{
-		Detector->detectMultiScale(Image, objects, scaleFactor, minNeighbours, 0, minObjSize, maxObjSize);
-	}
-
-	virtual ~CascadeDetectorAdapter()
-	{}
-
-private:
-	CascadeDetectorAdapter();
-	cv::Ptr<cv::CascadeClassifier> Detector;
-};
-
-int main(int, char**)
-{
-	namedWindow(WindowName);
-
-	VideoCapture VideoStream(0);
-
-	if (!VideoStream.isOpened())
-	{
-		printf("Error: Cannot open video stream from camera\n");
-		return 1;
-	}
-
-	std::string cascadeFrontalfilename = "../../data/lbpcascades/lbpcascade_frontalface.xml";
-	cv::Ptr<cv::CascadeClassifier> cascade = makePtr<cv::CascadeClassifier>(cascadeFrontalfilename);
-	cv::Ptr<DetectionBasedTracker::IDetector> MainDetector = makePtr<CascadeDetectorAdapter>(cascade);
-	if (cascade->empty())
-	{
-		printf("Error: Cannot load %s\n", cascadeFrontalfilename.c_str());
-		return 2;
-	}
-
-	cascade = makePtr<cv::CascadeClassifier>(cascadeFrontalfilename);
-	cv::Ptr<DetectionBasedTracker::IDetector> TrackingDetector = makePtr<CascadeDetectorAdapter>(cascade);
-	if (cascade->empty())
-	{
-		printf("Error: Cannot load %s\n", cascadeFrontalfilename.c_str());
-		return 2;
-	}
-
-	DetectionBasedTracker::Parameters params;
-	DetectionBasedTracker Detector(MainDetector, TrackingDetector, params);
-
-	if (!Detector.run())
-	{
-		printf("Error: Detector initialization failed\n");
-		return 2;
-	}
-
-	Mat ReferenceFrame;
-	Mat GrayFrame;
-	vector<Rect> Faces;
-
-	do
-	{
-		VideoStream >> ReferenceFrame;
-		cvtColor(ReferenceFrame, GrayFrame, COLOR_RGB2GRAY);
-		Detector.process(GrayFrame);
-		Detector.getObjects(Faces);
-
-		for (size_t i = 0; i < Faces.size(); i++)
+		while (true)
 		{
-			rectangle(ReferenceFrame, Faces[i], Scalar(0, 255, 0));
+			cap >> frame;
+
+			//-- 3. Apply the classifier to the frame
+			if (!frame.empty())
+			{
+				detectAndDisplay(frame);
+			}
+			else
+			{
+				printf(" --(!) No captured frame -- Break!"); break;
+			}
+
+			int c = waitKey(10);
+			if ((char)c == 'c') { break; }
 		}
-
-		imshow(WindowName, ReferenceFrame);
-	} while (waitKey(30) < 0);
-
-	Detector.stop();
-
+	}
 	return 0;
+}
+
+/** @function detectAndDisplay */
+void detectAndDisplay(Mat frame)
+{
+	std::vector<Rect> faces;
+	Mat frame_gray;
+
+	cvtColor(frame, frame_gray, CV_BGR2GRAY);
+	equalizeHist(frame_gray, frame_gray);
+	imshow(window_name, frame_gray);
+
+	//-- Detect faces
+	face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
+
+	for (size_t i = 0; i < faces.size(); i++)
+	{
+		Point center(faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5);
+		ellipse(frame, center, Size(faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
+
+		Mat faceROI = frame_gray(faces[i]);
+		std::vector<Rect> eyes;
+
+		//-- In each face, detect eyes
+		//eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
+
+		for (size_t j = 0; j < eyes.size(); j++)
+		{
+			Point center(faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5);
+			int radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
+			circle(frame, center, radius, Scalar(255, 0, 0), 4, 8, 0);
+		}
+	}
+	//-- Show what you got
+	imshow(window_name, frame);
 }
 
 
